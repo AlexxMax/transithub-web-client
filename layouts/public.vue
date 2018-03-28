a4aab05cc825c411017638210bdd09d04cfcf98a
Yuliia Shamatiienko <y@ione.software>
2018-03-28 15:34:13 +0300

    Added new flayout public

---
+++ layouts/public.vue
@@ -0,0 +1,42 @@
+// add navbar and footer
+
+<template>
+  <div>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">Processing Center</el-menu-item>
+        <el-menu-item index="2">Workspace</el-menu-item>
+        <el-menu-item index="3" disabled>Info</el-menu-item>
+        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
+    </el-menu>
+    <div class="line"></div>
+    <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#f0b917"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1">Processing Center</el-menu-item>
+        <el-menu-item index="2">Workspace</el-menu-item>
+        <el-menu-item index="3" disabled>Info</el-menu-item>
+        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+      activeIndex2: "1"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
