f723ce211cee4a2965b8a0f2d578ddd0f7b553c3
Yuliia Shamatiienko <y@ione.software>
2018-04-05 16:10:48 +0300

    Added colums in reg. form for desktop version

--- pages/_LANG/registration/index.vue
+++ pages/_LANG/registration/index.vue
@@ -1,42 +1,68 @@
 <template>
   <div>
-    <el-row type="flex" justify="center">
+    <el-row :gutter="40" type="flex" justify="center">
 
       <!-- Card -->
-      <el-col :xs="18" :sm="16" :md="14" :lg="8" :xl="8">
+      <el-col :xs="18" :sm="16" :md="14" :lg="14" :xl="14">
         <el-card class="box-card">
 
           <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" class="demo-ruleForm">
             <span class="th-form-title">Реєстрація</span>
 
-            <el-form-item prop="fullName">
-              <label>Прізвище, ім'я, по батькові *</label>
-              <el-input v-model.number="ruleForm2.fullName" placeholder="Введіть повне ім'я"></el-input>
-            </el-form-item>
+            <el-col :xs="24" :sm="24" :md="24" :lg="12" :xl="12">
+              <el-form-item prop="surname">
+                <label>Прізвище *</label>
+                <el-input v-model.number="ruleForm2.name" placeholder="Введіть прізвище"></el-input>
+              </el-form-item>
 
-            <el-form-item prop="email">
-              <label>Номер електронної пошти *</label>
-              <el-input type='email' v-model.number="ruleForm2.email" placeholder="Введіть електронну пошту"></el-input>
-            </el-form-item>
+              <el-form-item prop="name">
+                <label>Ім'я *</label>
+                <el-input v-model.number="ruleForm2.name" placeholder="Введіть ім'я"></el-input>
+              </el-form-item>
 
-            <el-form-item prop="number">
-              <label>Електронна пошта *</label>
-              <el-input v-model.number="ruleForm2.number" placeholder="Введіть номер моб. телефону"></el-input>
-            </el-form-item>
+              <el-form-item prop="patronymic">
+                <label>По батькові *</label>
+                <el-input v-model.number="ruleForm2.name" placeholder="Введіть по батькові"></el-input>
+              </el-form-item>
 
-            <el-form-item prop="pass">
-              <label>Пароль *</label>
-              <el-input type="password" v-model="ruleForm2.pass" auto-complete="off" placeholder="Введіть пароль"></el-input>
-            </el-form-item>
+              <el-form-item prop="email">
+                <label>Електронна пошта *</label>
+                <el-input type='email' v-model.number="ruleForm2.email" placeholder="Введіть електронну пошту"></el-input>
+              </el-form-item>
 
-            <el-form-item prop="checkPass">
-              <label>Підтвердження пароля *</label>
-              <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="Підтвердьте пароль"></el-input>
-            </el-form-item>
+            </el-col>
 
-            <div class="th-btn-submit-wrapper">
-              <button class="th-btn-submit" @click="submitForm('ruleForm2')">Зареєструватися</button>
-            </div>
+            <el-col :xs="24" :sm="24" :md="24" :lg="12" :xl="12">
+
+              <el-form-item prop="number">
+                <label>Номер моб. телефону *</label>
+                <el-input v-model.number="ruleForm2.number" placeholder="Введіть номер моб. телефону"></el-input>
+              </el-form-item>
+
+              <el-form-item prop="pass">
+                <label>Пароль *</label>
+                <el-input type="password" v-model="ruleForm2.pass" auto-complete="off" placeholder="Введіть пароль"></el-input>
+              </el-form-item>
+
+              <el-form-item prop="checkPass">
+                <label>Підтвердження пароля *</label>
+                <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="Підтвердьте пароль"></el-input>
+              </el-form-item>
+
+              <el-form-item>
+                <div class="th-note">
+                  <span>* обов'язкові поля</span>
+                </div>
+              </el-form-item>
+
+            </el-col>
+
+            <el-col>
+              
+              <div class="th-btn-submit-wrapper">
+                <button class="th-btn-submit" @click="submitForm('ruleForm2')">Зареєструватися</button>
+              </div>
+            </el-col>
 
           </el-form>
         </el-card>
@@ -48,11 +74,15 @@
 </template>
 
 <style lang="scss" scoped>
+.el-row {
+  margin: 0 !important;
+}
+
 .el-card {
   padding: 20px 40px;
   background: white;
   z-index: 100;
-  margin-top: -80px;
+  margin: -80px 0 80px 0;
 
   .th-form-title {
     font-size: 34px;
@@ -73,25 +103,14 @@
     }
   }
 
-  .th-remember,
-  .th-registration {
-    .el-checkbox,
-    span,
-    a {
-      font-size: 14px !important;
-      color: #989795;
-    }
-    a {
-      float: right;
-
-      &:hover {
-        color: #f4c333;
-      }
-    }
+  .th-note {
+    font-size: 12px !important;
+    color: #989795;
   }
+
   .th-btn-submit-wrapper {
     width: 100%;
-    margin-top: 44px;
+    margin: 20px 0 40px 0;
 
     .th-btn-submit {
       width: 100%;
@@ -107,14 +126,6 @@
       }
     }
   }
-
-  .th-registration {
-    margin-top: 44px;
-    a {
-      color: #f0b917;
-      font-size: 16px !important;
-    }
-  }
 }
 </style>
 
@@ -123,9 +134,25 @@ export default {
   layout: "authorization",
 
   data() {
-    var checkFullName = (rule, value, callback) => {
+    var checkSurname = (rule, value, callback) => {
       if (!value) {
-        return callback(new Error("Будь ласка, введіть повне ім'я"));
+        return callback(new Error("Будь ласка, введіть прізвище"));
+      } else {
+        callback();
+      }
+    };
+
+    var checkName = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("Будь ласка, введіть ім'я"));
+      } else {
+        callback();
+      }
+    };
+
+    var checkPatronymic = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("Будь ласка, введіть по батькові"));
       } else {
         callback();
       }
@@ -175,7 +202,9 @@ export default {
 
     return {
       ruleForm2: {
-        fullName: "",
+        surname: "",
+        name: "",
+        patronymic: "",
         email: "",
         number: "",
         pass: "",
@@ -183,9 +212,23 @@ export default {
       },
 
       rules2: {
-        fullName: [
+        surname: [
           {
-            validator: checkFullName,
+            validator: checkSurname,
+            trigger: "blur"
+          }
+        ],
+
+        name: [
+          {
+            validator: checkName,
+            trigger: "blur"
+          }
+        ],
+
+        patronymic: [
+          {
+            validator: checkPatronymic,
             trigger: "blur"
           }
         ],
