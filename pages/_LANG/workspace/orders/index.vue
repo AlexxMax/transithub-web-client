5a851cfa162cfc4487f8b5b04db65b0b7920756f
Yuliia Shamatiienko <y@ione.software>
2018-05-02 12:57:59 +0300

    Added orders table

--- pages/_LANG/workspace/orders/index.vue
+++ pages/_LANG/workspace/orders/index.vue
@@ -1,19 +1,21 @@
 <template>
   <el-row>
     <el-table :data="orders" stripe style="width: 100%">
-      <el-table-column prop="created_at" label="Дата та час створення замовлення" width="270">
+      <el-table-column prop="number" label="№" width="50">
       </el-table-column>
-      <el-table-column prop="owner" label="Власник" width="155">
+      <el-table-column prop="created_at" label="Дата та час створення замовлення" width="265">
       </el-table-column>
-      <el-table-column prop="client" label="Клієнт" width="155">
+      <el-table-column prop="owner" label="Власник" width="140">
       </el-table-column>
-      <el-table-column prop="point_from_name" label="Пункт завантаження" width="195">
+      <el-table-column prop="client" label="Клієнт" width="140">
       </el-table-column>
-      <el-table-column prop="point_to_name" label="Пункт розвантаження" width="195">
+      <el-table-column prop="point_from_name" label="Пункт завантаження" width="185">
       </el-table-column>
-      <el-table-column prop="warehouse_from_name" label="Склад завантаження" width="260">
+      <el-table-column prop="point_to_name" label="Пункт розвантаження" width="185">
       </el-table-column>
-      <el-table-column prop="warehouse_to_name" label="Склад розвантаження" width="260">
+      <el-table-column prop="warehouse_from_name" label="Склад завантаження" width="265">
+      </el-table-column>
+      <el-table-column prop="warehouse_to_name" label="Склад розвантаження" width="265">
       </el-table-column>
     </el-table>
   </el-row>
