6dfef53f7daa86546db0de0d2e9e08582ee85f56
Yaroslav Shamatienko <ShamatienkoYaroslav@users.noreply.github.com>
2018-05-24 13:37:26 +0300

    Oreder

--- components/Common/Form.vue
+++ components/Common/Form.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
     <div class="th-details-form">
-      <slot></slot>
+      <slot name="header"></slot>
+
+      <slot name="toolbar"></slot>
+
+      <div class="th-form-content">
+        <slot name="content"></slot>
+      </div>
+
+      <slot name="footer"></slot>
     </div>
   </div>
 </template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+  .th-form-content {
+    margin-top: 20px;
+  }
+</style>
+
---
+++ components/Orders/FormElement.vue
@@ -0,0 +1,42 @@
+<template>
+  <th-form>
+    <template slot="header">
+      <h3>{{ order.title }}</h3>
+    </template>
+
+    <template slot="content">
+      <el-form label-position="top" label-width="100px" :model="order" size="mini">
+        <el-form-item label="Client">
+          <el-input v-model="order.client"></el-input>
+        </el-form-item>
+
+        <el-form-item label="Goods">
+          <el-input v-model="order.goods_name"></el-input>
+        </el-form-item>
+      </el-form>
+    </template>
+  </th-form>
+</template>
+
+<script>
+import CommonForm from '@/components/Common/Form'
+
+export default {
+  components: {
+    "th-form": CommonForm
+  },
+
+  data() {
+    const order = this.$store.state.orders.list.find(elem => elem.guid === this.$route.params.guid)
+    return {
+      order: {
+        ...order
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
