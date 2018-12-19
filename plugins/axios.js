3fbd65490d71d0a0b683d0191a4a65e395cfa67e
Yaroslav Shamatienko <ShamatienkoYaroslav@users.noreply.github.com>
2018-12-19 17:51:43 +0200

    axios param

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,12 @@
 import globalConfig from '@/config'
 
 export default function ({ $axios }) {
-  $axios.setHeader('client_token', globalConfig.backend.logistics.token)
+  // $axios.setHeader('client_token', globalConfig.backend.logistics.token)
 
   $axios.onRequest(config => {
+    const params = config.params || {}
+    params.client_token = globalConfig.backend.logistics.token
+    config.params = params
     console.log('Making request to backend with ' + config.url)
   })
 
