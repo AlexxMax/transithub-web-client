30b8183e6e4005ffc45bd8eeb0484744727c6291
Yaroslav Shamatienko <ShamatienkoYaroslav@users.noreply.github.com>
2019-06-14 16:58:01 +0300

    ESA-2089

--- utils/google/maps/models/direction.js
+++ utils/google/maps/models/direction.js
@@ -1,7 +1,5 @@
 export default class Direction {
   constructor(google, map, origin, destination) {
-    console.log("TCL: Direction -> constructor -> destination", destination)
-    console.log("TCL: Direction -> constructor -> origin", origin)
     this.mode = 'DRIVING'
 
     this.google = google
@@ -20,7 +18,6 @@ export default class Direction {
 
     const self = this
     this.service.route(request, (response, status) => {
-      console.log("TCL: Direction -> constructor -> status", status)
       if (status === 'OK') {
         self.renderer.setDirections(response)
       }
